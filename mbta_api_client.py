a45efba6cc22b7833147530261edbe5648bc7101
Ned Murphy <ned@thelevelup.com>
2018-04-10 18:51:49 -0400

    Hooking up wait times code

--- mbta_api_client.py
+++ mbta_api_client.py
@@ -4,16 +4,34 @@ import json
 class MbtaApi3Client:
     url_base = 'https://api-v3.mbta.com'
     neds_api_key = '5e44686f7e394dab992135e893b8a9f4'
-    routeIds = {'orange': 'Orange', 'red': 'Red'}
-    stopIds = {'downtown crossing': 1234, 'stony brook': 70005}
+    routeIds = {
+        'orange': 'Orange',
+        'red': 'Red',
+        'blue': 'Blue',
+        'green_b': 'Green-B',
+        'green_c': 'Green-C',
+        'green_d': 'Green-D',
+        'green_e': 'Green-E'
+    }
+    stopIds = {
+        'downtown_crossing_alewife': 70078,
+        'downtown_crossing_ashmont_braintree': 70077,
+        'downtown_crossing_forest_hills': 70020,
+        'downtown_crossing_oak_grove': 70021,
+        'stony_brook_oak_gove': 70005,
+        'stony_brook_forest_hills': 70004,
+        'state_street_oak_grove': 70023,
+        'state_street_forest_hills': 70022,
+        'state_street_bowdoin': 70041,
+        'state_street_wonderland': 70042
+    }
 
     def __init__(self, apiKey):
         self.apiKey = apiKey
-        self.directionID = 1
 
     def getPredictions(self, routeID, stopID, directionID = None):
         predictionsUrl = '{}/predictions?filter[route]={}&filter[stop]={}&filter[direction_id]={}'
-        url = predictionsUrl.format(self.url_base, routeID, stopID, self.directionID)
+        url = predictionsUrl.format(self.url_base, routeID, stopID, directionID)
 
         predictions = []
         try:
@@ -24,5 +42,5 @@ class MbtaApi3Client:
         return predictionsJson['data']
 
     def getArrivalTimePredictions(self, routeID, stopID, directionID = None):
-        predictions = getPredictions(routeID, stopID, directionID)
+        predictions = self.getPredictions(routeID, stopID, directionID)
         return [p['attributes']['arrival_time'].encode('utf-8') for p in predictions]
--- mbta_shiftregister_display.py
+++ mbta_shiftregister_display.py
@@ -1,6 +1,9 @@
 from mbta_api_client import MbtaApi3Client
-import time
-import datetime
+from dateutil import parser
+from pytz import timezone
+import pytz
+import math
+from datetime import datetime
 from shiftregister import ShiftRegister
 
 def get_register():
@@ -9,12 +12,31 @@ def get_register():
 def clear_register(register):
   register.values = [0,0,0,0,0,0,0,0]
 
+def get_wait_times(api_client, train_line, stop):
+  predicted_arrival_times = client.getArrivalTimePredictions(MbtaApi3Client.routeIds[train_line],
+    MbtaApi3Client.stopIds[stop])
+
+  utc_parsed_times = [parser.parse(pat).astimezone(pytz.utc) for pat in predicted_arrival_times]
+
+  return [math.trunc((t - datetime.now(pytz.utc)).seconds/60) for t in utc_parsed_times]
+
 
 client = MbtaApi3Client(MbtaApi3Client.neds_api_key)
 
-now = datetime.datetime.now()
-now_time = now.strftime("%H:%M")
+utc_now = datetime.now(pytz.utc)
 
 predicted_arrival_times = client.getArrivalTimePredictions(MbtaApi3Client.routeIds['orange'],
-  MbtaApi3Client.stopIds['downtown crossing'])
+  MbtaApi3Client.stopIds['downtown_crossing_forest_hills'])
 
+utc_parsed_times = [parser.parse(pat).astimezone(pytz.utc) for pat in predicted_arrival_times]
+
+wait_times_in_min = [math.trunc((t - utc_now).seconds/60) for t in utc_parsed_times]
+
+print("Wait times at Downtown Crossing for Orange line to Forest Hills are {}".format(get_wait_times(client, 'orange', 'downtown_crossing_forest_hills')))
+print("Wait times at Downtown Crossing for Orange line to Oak Grove are {}".format(get_wait_times(client, 'orange', 'downtown_crossing_oak_grove')))
+print("Wait times at Downtown Crossing for Red line to Alewife are {}".format(get_wait_times(client, 'red', 'downtown_crossing_alewife')))
+print("Wait times at Downtown Crossing for Red line to Ashmont/Braintree are {}".format(get_wait_times(client, 'red', 'downtown_crossing_ashmont_braintree')))
+print("Wait times at State Street for Orange line to Forest Hills are {}".format(get_wait_times(client, 'orange', 'state_street_forest_hills')))
+print("Wait times at State Street for Orange line to Oak Grove are {}".format(get_wait_times(client, 'orange', 'state_street_oak_grove')))
+print("Wait times at State Street for Blue line to Bowdoin are {}".format(get_wait_times(client, 'blue', 'state_street_bowdoin')))
+print("Wait times at State Street for Blue line to Wonderland are {}".format(get_wait_times(client, 'blue', 'state_street_wonderland')))
